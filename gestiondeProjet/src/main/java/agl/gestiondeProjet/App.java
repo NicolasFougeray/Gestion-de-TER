1654948550699f972a48cd4fc207352c79d71684
NicolasFougeray <93988044+NicolasFougeray@users.noreply.github.com>
2021-11-09 13:57:29 +0100

    First push

---
+++ gestiondeProjet/src/main/java/agl/gestiondeProjet/App.java
@@ -0,0 +1,13 @@
+package agl.gestiondeProjet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
